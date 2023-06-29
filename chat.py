b0ab35186a0124c1acdba82c3ae4d67f2d550901
Staroom <31646341+eastmoe@users.noreply.github.com>
2023-06-29 22:09:44 +0800

    更换触发词

--- chat.py
+++ chat.py
@@ -29,26 +29,26 @@ class ChatPlugin(Plugin):
     historydata = {'internal': [], 'visible': []}
 
     # 测试，收到唤醒，发送回复醒了
-    @listen_to("唤醒")
+    @listen_to("*唤醒")
     async def wake_up(self, message: Message):
         self.driver.reply_to(message, "我醒了！")
 
 
     # 复读功能测试
-    @listen_to('Give me (.*)')
+    @listen_to('*Give me (.*)')
     async def give_me(self, message, something):
         self.driver.reply_to(message, 'Here is %s' % something)
 
-    @listen_to("帮助")
+    @listen_to("*help")
     async def help_reply(self, message):
         self.driver.create_post(channel_id=message.channel_id, message=
         '## 使用帮助'
         +'\n发送“**对话 + 空格 + 内容**”，即可与机器人对话；'
-        +'\n发送“**重置**”可重置当前对话的历史记忆；'
-        +'\n发送“**模型列表**”可查看对应的text-generation webui的模型列表；'
-        +'\n发送“**当前模型**”可查看当前模型信息；'
-        +'\n发送“**切换模型 + 空格 + 完整的模型名称**”可切换模型；'
-        +'\n发送“**帮助**”可返回本文本。'
+        +'\n发送“***reset**”可重置当前对话的历史记忆；'
+        +'\n发送“***modellist**”可查看对应的text-generation webui的模型列表；'
+        +'\n发送“***model**”可查看当前模型信息；'
+        +'\n发送“***switchmodelto + 空格 + 完整的模型名称**”可切换模型；'
+        +'\n发送“***help**”可返回本文本。'
         )
 
 
@@ -66,7 +66,7 @@ class ChatPlugin(Plugin):
         # 直接发布主题回复用户
         self.driver.create_post(channel_id=message.channel_id, message='%s' % TestPlugin.outtext)
 
-    @listen_to("重置")
+    @listen_to("*reset")
     async def reset(self,message):
         # 定义对话历史记录及上次输出内容为空
         ChatPlugin.logarr = []
@@ -75,7 +75,7 @@ class ChatPlugin(Plugin):
         #提示重置完成
         self.driver.create_post(channel_id=message.channel_id, message='**对话已被重置。**')
 
-    @listen_to("模型列表")
+    @listen_to("*modellist")
     async def model_info(self,message):
         #获取模型信息
         modellist=tgapi_model.model_api({'action': 'list'})['result']
@@ -87,7 +87,7 @@ class ChatPlugin(Plugin):
         # 输出模型列表
         self.driver.create_post(channel_id=message.channel_id,message='所有模型：\n'+modellisttext)
 
-    @listen_to('当前模型')
+    @listen_to('*model')
     async def present_modefl_info(self,message):
         #发起一个模型API，获得响应
         resp = tgapi_model.model_api({'action': 'info'})
@@ -97,7 +97,7 @@ class ChatPlugin(Plugin):
         self.driver.create_post(channel_id=message.channel_id,message= '当前模型：' + str(resp['result']['model_name'])+'\n加载的LoRa们：' +str(resp['result']['lora_names'])+'\n输入的截断长度(truncation_length):'+str(resp['result']['shared.settings']['truncation_length']) +'\n提示词模板(instruction_template)：'+str(resp['result']['shared.settings']['instruction_template']) )
 
     # 切换模型
-    @listen_to('切换模型 (.*)')
+    @listen_to('*switchmodelto (.*)')
     async def switch_model(self, message, something):
         # 获取模型信息
         #modellist = tgapi_model.model_api({'action': 'list'})['result']
